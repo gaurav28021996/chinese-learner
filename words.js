557e0d671fb153cc987584dd636f818fcac592b9
CHANDRA BHARAT <gk44678@gmail.com>
2025-06-09 23:46:18 +0530

    Create words.js

---
+++ words.js
@@ -0,0 +1,41 @@
+// words.js
+
+// This is our database of words.
+const words = [
+    {
+        character: "你好",
+        pinyin: "nǐ hǎo",
+        translation: "Hello",
+        search_term: "greeting hello" // A good term for image search
+    },
+    {
+        character: "谢谢",
+        pinyin: "xiè xie",
+        translation: "Thank you",
+        search_term: "gratitude thank you"
+    },
+    {
+        character: "家",
+        pinyin: "jiā",
+        translation: "Home / Family",
+        search_term: "cozy family home"
+    },
+    {
+        character: "猫",
+        pinyin: "māo",
+        translation: "Cat",
+        search_term: "cute cat"
+    },
+    {
+        character: "狗",
+        pinyin: "gǒu",
+        translation: "Dog",
+        search_term: "happy dog"
+    },
+    {
+        character: "爱",
+        pinyin: "ài",
+        translation: "Love",
+        search_term: "love heart symbol"
+    }
+];
